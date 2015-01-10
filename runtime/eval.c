e1f03ea5fb3d37abf9fbf4172bf50bdd925ae826
k-stachowiak <krzysiek.stachowiak@gmail.com>
2015-01-10 12:32:32 +0100

    Factored the evaluation of compound AST nodes out to another module.

--- runtime/eval.c
+++ runtime/eval.c
@@ -54,40 +54,6 @@ void eval_error_not_found(char *symbol)
 	err_msg_set(&msg);
 }
 
-static void eval_compound(
-        struct AstNode *node,
-        struct Runtime *rt,
-        struct SymMap *sym_map)
-{
-    VAL_LOC_T size_loc, data_begin, data_size;
-    struct AstNode *current = node->data.compound.exprs;
-
-	/* Header. */
-    switch (node->data.compound.type) {
-        case AST_CPD_ARRAY:
-			rt_val_push_array_init(rt->stack, &size_loc);
-            break;
-
-        case AST_CPD_TUPLE:
-			rt_val_push_tuple_init(rt->stack, &size_loc);
-            break;
-    }
-
-    /* Data. */
-    data_begin = rt->stack->top;
-    while (current) {
-        eval_impl(current, rt, sym_map);
-        if (err_state()) {
-            return;
-        }
-        current = current->next;
-    }
-
-    /* Hack value size to correct value. */
-    data_size = rt->stack->top - data_begin;
-	rt_val_push_cpd_final(rt->stack, size_loc, data_size);
-}
-
 static void eval_literal(struct AstNode *node, struct Stack *stack)
 {
     switch (node->data.literal.type) {
