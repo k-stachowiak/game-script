9cec8425e2cafdca0fbb940e15c25f43bbf1cb46
k.stachowiak <k.stachowiak@samsung.com>
2015-04-16 15:20:26 +0200

    Adding a test case for the CCI

--- client/test/test_cci.c
+++ client/test/test_cci.c
@@ -4,7 +4,15 @@
 
 static void test_cci_impl(struct TestContext *tc, struct Runtime *rt)
 {
-
+    test_eval_source_succeed(tc, rt,
+        "(bind sqr (func (x) (* x x)))\n"
+        "(bind point (func (f g x) (g (f x))))",
+        "Prepare context for test");
+    test_eval_source_succeed(tc, rt,
+        "(bind tetr (point sqr sqr))",
+        "Defining function as composition");
+    test_eval_source_expect(tc, rt,
+        "(tetr 2)", "Evaluate", INT, 16);
 }
 
 void test_cci(struct TestContext *tc)
