5585343f38ad6fe2a95e3308b8a34931828020b1
k-stachowiak <krzysiek.stachowiak@gmail.com>
2015-09-07 10:54:53 +0200

    Removed obsolete translation unit; moved do block evaluation to the eval.c file

--- runtime/eval/eval.c
+++ runtime/eval/eval.c
@@ -52,6 +52,34 @@ static void eval_literal(struct AstNode *node, struct Stack *stack)
     }
 }
 
+static void eval_do_block(
+        struct AstNode *node,
+        struct Runtime *rt,
+        struct SymMap *sym_map)
+{
+    struct SymMap local_sym_map;
+    struct AstNode *expr = node->data.do_block.exprs;
+
+    sym_map_init_local(&local_sym_map, sym_map);
+
+    VAL_LOC_T begin = rt->stack.top;
+    VAL_LOC_T end = rt->stack.top;
+
+    for (; expr; expr = expr->next) {
+        VAL_LOC_T new_end = eval_impl(expr, rt, &local_sym_map);
+        if (err_state()) {
+            err_push_src("EVAL", node->loc, "Failed evaluating do expression");
+            break;
+        } else {
+            end = new_end;
+        }
+    }
+
+    stack_collapse(&rt->stack, begin, end);
+
+    sym_map_deinit(&local_sym_map);
+}
+
 static void eval_reference(
         struct AstNode *node,
         struct Stack *stack,
--- runtime/eval/eval_detail.h
+++ runtime/eval/eval_detail.h
@@ -8,7 +8,6 @@ void eval_error_not_found(char *symbol);
 void eval_func_def(struct AstNode *node, struct Stack *stack, struct SymMap *sym_map);
 void eval_func_call(struct AstNode *node, struct Runtime *rt, struct SymMap *sym_map);
 void eval_compound(struct AstNode *node, struct Runtime *rt, struct SymMap *sym_map);
-void eval_do_block(struct AstNode *node, struct Runtime *rt, struct SymMap *sym_map);
 void eval_parafunc(struct AstNode *node, struct Runtime *rt, struct SymMap *sym_map);
 void eval_bind(struct AstNode *node, struct Runtime *rt, struct SymMap *sym_map);
 
